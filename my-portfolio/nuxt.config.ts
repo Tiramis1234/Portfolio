011bf11b26a5dbc553533d6062233efbc847e809
Marek Lenarczyk <marek.lenarczyk2004@gmail.com>
2026-04-27 21:32:37 +0200

    asd

--- my-portfolio/nuxt.config.ts
+++ my-portfolio/nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  
+  app: {
+    baseURL: '/portfolio/',
+    buildAssetsDir: 'assets'
+  },
+
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss'],
-
   ssr: true
 })
